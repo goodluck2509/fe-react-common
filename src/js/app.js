dd1813576c3c32b28ff38ec2ac13fa2731a141f3
Cong quy <congquy2509@gmail.com>
2022-03-28 09:20:58 +0700

    updatePublishNpm

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
-import { Pop1 } from "./a.js";
-import { Pop2 } from "./b.js";
-
-function Abc() {
-  alert("Abc");
+function isWds(string) {
+  return string === "WDS";
 }
 
-export { Pop1, Pop2, Abc };
+module.exports = isWds;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require("path");
+const path = require("path"); // dùng node_modules 
 
 const devMode = true;
 
-module.exports = {
+module.exports = { // thực ra nó là export default của ES6 còn đây cú pháp của node 5
   mode: devMode ? "development" : "production",
   entry: [
     "./src/js/app.js", // file nguồn Webpack làm việc
+    // "./src/TextField/index.js", // file nguồn Webpack làm việc
   ],
 
   output: {
@@ -19,77 +20,85 @@ module.exports = {
     // We no not want to minimize our code.
     //minimize: false
   },
-
-  module: {
-    rules: [
-      {
-        // Thiết lập build scss
-        test: /\.(sa|sc)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            // Interprets CSS
-            loader: "css-loader",
-            options: {
-              importLoaders: 2,
-            },
-          },
-          {
-            // minify CSS và thêm autoprefix
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-
-              // Đặt chế độ tối ưu
-              plugins: devMode
-                ? () => []
-                : () => [
-                    postcssPresetEnv({
-                      browsers: [">1%"],
-                    }),
-                    require("cssnano")(),
-                  ],
-            },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      {
-        // Thiết lập lưu các ảnh sử dụng bởi CSS
-        // lưu dưới đường dẫn images cùng file site.css
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              // Image sử dụng bởi CSS lưu tại
-              publicPath: "../images",
-              emitFile: false,
-            },
-          },
-        ],
-      },
-    ],
+  resolve: {
+    alias: {
+      // react: path.resolve('./node_modules/react'),
+      "@material-ui/core": path.resolve("./node_modules/@material-ui/core"),
+      "@material-ui/core/styles": path.resolve(
+        "./node_modules/@material-ui/core/styles"
+      ),
+    },
   },
+  // module: {
+  //   rules: [
+  //     {
+  //       // Thiết lập build scss
+  //       test: /\.(sa|sc)ss$/,
+  //       use: [
+  //         {
+  //           loader: MiniCssExtractPlugin.loader,
+  //         },
+  //         {
+  //           // Interprets CSS
+  //           loader: "css-loader",
+  //           options: {
+  //             importLoaders: 2,
+  //           },
+  //         },
+  //         {
+  //           // minify CSS và thêm autoprefix
+  //           loader: "postcss-loader",
+  //           options: {
+  //             ident: "postcss",
 
-  plugins: [
-    // Xuất kết quả với CSS - sau khi qua loader MiniCssExtractPlugin.loader
-    new MiniCssExtractPlugin({
-      filename: devMode ? "css/site.css" : "css/site.min.css",
-    }),
-    // Copy file với copy-webpack-plugin
-    // syntax: `npm install copy-webpack-plugin`
+  //             // Đặt chế độ tối ưu
+  //             plugins: devMode
+  //               ? () => []
+  //               : () => [
+  //                   postcssPresetEnv({
+  //                     browsers: [">1%"],
+  //                   }),
+  //                   require("cssnano")(),
+  //                 ],
+  //           },
+  //         },
+  //         {
+  //           loader: "sass-loader",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       // Thiết lập lưu các ảnh sử dụng bởi CSS
+  //       // lưu dưới đường dẫn images cùng file site.css
+  //       test: /\.(png|jpe?g|gif)$/,
+  //       use: [
+  //         {
+  //           loader: "file-loader",
+  //           options: {
+  //             name: "[name].[ext]",
+  //             // Image sử dụng bởi CSS lưu tại
+  //             publicPath: "../images",
+  //             emitFile: false,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 
-    // new CopyPlugin([
-    //   { from: 'node_modules/bootstrap/dist/js/bootstrap.js', to: 'js/bootstrap.js' },
-    //   { from: 'node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'js/bootstrap.min.js' },
-    // ]),
-  ],
+  // plugins: [
+  //   // Xuất kết quả với CSS - sau khi qua loader MiniCssExtractPlugin.loader
+  //   new MiniCssExtractPlugin({
+  //     filename: devMode ? "css/site.css" : "css/site.min.css",
+  //   }),
+  //   // Copy file với copy-webpack-plugin
+  //   // syntax: `npm install copy-webpack-plugin`
+
+  //   // new CopyPlugin([
+  //   //   { from: 'node_modules/bootstrap/dist/js/bootstrap.js', to: 'js/bootstrap.js' },
+  //   //   { from: 'node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'js/bootstrap.min.js' },
+  //   // ]),
+  // ],
 };
 // Các loader thực hiện từ dưới lên trên,
 // như ở ví dụ trên đối với file CSS nó sẽ chuyển đổi dần qua các loader:
